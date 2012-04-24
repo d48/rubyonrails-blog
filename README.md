2d5d4584d64487681d9b543cbfaeb6d90e84f31b
Ryan Regalado <ryan@design48.net>
2012-04-24 00:59:50 -0700

    first commit and new README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ruby on Rails blog
+
+Assembler for Ruby on Rails for a blog application.
+
+## Roadmap
+
+* ~~Setup up local environment
+* Create scaffold for post resource
+* Run a migration for creating/modifying sqlite3 database tables
+* Set up CRUD operations for posts
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlog::Application.config.secret_token = '4bc1512da596339a6cdb3af8402eeaf08e5e4676fb1b0f261ac90492b4530fd4f815c228cab6d36186faa6f278d3d941150ccf9d89f9d20ab3cae3f5767f5c32'
